a5b0a1b7107b38338c26f11c88515c3b64650e76
Michael van Vliet <info@michaelvanvliet.nl>
2013-05-30 11:08:16 +0200

    Split header, content and footer using PHP

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+      <hr>
+      <div class="footer">
+        <p>&copy; 2009-2013 Netherlands Metabolomics Centre</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+  </body>
+</html>
